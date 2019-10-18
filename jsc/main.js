c967720b75598853c894dd27293b09e7bf23c309
jabuarab <alasjan1@gmail.com>
2019-10-18 13:16:22 -0300

    61 enter leave click con errores chotos arreglasdos falta centrar y hacer lo del cet y los botones de obligatoria o obcionales

--- jsc/main.js
+++ jsc/main.js
@@ -1,269 +1,312 @@
-/*let materias=[
-  {"id": "analisis2",
-    "hijo": ["analisis1"]
-  },
-  {"id":"analisis1" ,
-    "hijo":[]
-  },
-  {"id":"gal1" ,
-    "hijo":[]
-  },
-  { "id": "fisica1",
-    "hijo":[]
-  },
-  {"id":"intRob"	 ,
-    "hijo":[]
-  },
-  { "id": "intIng",
-    "hijo":[]
-  },
-  {"id": "gal2",
-    "hijo":["gal1"]
-  },
-  { "id": "fisica2",
-    "hijo":[]
-  },
-  { "id": "matlab",
-    "hijo":[]
-  },
-  { "id": "funQui",
-    "hijo":[]
-  },
-  { "id": "caluclo",
-    "hijo":["analisis2","gal2"]
-  },
-  { "id": "analisis3",
-    "hijo":["analisis2","gal2"]
-  },
-  { "id": "mecanica1",
-    "hijo":["fisica1","analisis1"]
-  },
-  { "id": "cad",
-    "hijo":[]
-  },
-  {
-    "id": "labInd",
-    "hijo":[]
-  },
-  {
-    "id": "ch1",
-    "hijo":[]
-  },
-  {
-    "id": "prob",
-    "hijo":["analisis2"]
-  },
-  {
-    "id": "mecanica2",
-    "hijo":["mecanica1"]
-  },
-  {
-    "id": "termodinamica",
-    "hijo":["analisis1"]
-  },
-  {
-    "id": "resMat",
-    "hijo":["mecanica1"]
-  },
-  {
-    "id": "ayudaTecnica",
-    "hijo":["cad"]
-  },
-  {
-    "id": "eco",
-    "hijo":[]
-  },
-  {
-    "id": "comProf",
-    "hijo":[]
-  },
-  {
-    "id": "ivestOper",
-    "hijo":["prob"]
-  },
-  {
-    "id": "electroMag",
-    "hijo":["fisica2","analisis3"]
-  },
-  {
-    "id": "pasantia1",
-    "hijo":[]
-  },
-  {
-    "id": "resMat2",
-    "hijo":["resMat"]
-  },
-  {
-    "id": "ceinIngMat",
-    "hijo":["funQui"]
-  },
-  {
-    "id": "labInd2",
-    "hijo":["labInd"]
-  },
-  {
-    "id": "contYcost",
-    "hijo":[]
-  },
-  {
-    "id": "ivestOper2",
-    "hijo":["ivestOper"]
-  },
-  {
-    "id": "Electrotecnia1",
-    "hijo":["electroMag"]
-  },
-  {
-    "id": "mecFludis",
-    "hijo":["analisis3","pasantia1","termodinamica"]
-  },
-  {
-    "id": "Metalurgia",
-    "hijo":["ceinIngMat"]
-  },
-  {
-    "id": "teologia",
-    "hijo":[]
-  },
-  {
-    "id": "cet1",
-    "hijo":[]
-  },
-  {
-    "id": "gestCal",
-    "hijo":[]
-  },
-  {
-    "id": "operProd1",
-    "hijo":["ivestOper"]
-  },
-  {
-    "id": "Electrotecnia2",
-    "hijo":["Electrotecnia1"]
-  },
+let materias = [
     {
-    "id": "pasantia2",
-    "hijo":[""]
-  },
-  {
-    "id": "transfCalor",
-    "hijo":["termodinamica"]
-  },
-  {
-    "id": "ingAmbiental",
-    "hijo":[]
-  },
-  {
-    "id": "maq1",
-    "hijo":["mecFludis","mecanica2"]
-  },
-  {
-    "id": "cet2",
-    "hijo":[""]
-  },
-  {
-    "id": "maq2",
-    "hijo":["pasantia2","transfCalor"]
-  },
-  {
-    "id": "Electronica",
-    "hijo":["electroMag"]
-  },
-  {
-    "id": "prosUnitarios",
-    "hijo":["funQui"]
-  },
-  {
-    "id": "labInd3",
-    "hijo":["labInd2"]
-  },
-  {
-    "id": "ch2",
-    "hijo":[]
-  },
-  {
-    "id": "autoControlIndustrial",
-    "hijo":["Electronica"]
-  },
-  {
-    "id":"diseElementMaquinas" ,
-    "hijo":["resMat2"]
-  },
-  {
-    "id": "pasantia3",
-    "hijo":[]
-  },
-  {
-    "id": "maq3",
-    "hijo":["maq2"]
-  },
-  {
-    "id": "proyInven",
-    "hijo":["contYcost"]
-  },
-  {
-    "id": "cet3",
-    "hijo":[]
-  },
-  {
-    "id":"cet4" ,
-    "hijo":[]
-  },
-  {
-    "id": "proyInd",
-    "hijo":["Electrotecnia2","maq3"]
-  },
-  {
-    "id": "elemGestion",
-    "hijo":["eco"]
-  },
-  {
-    "id": "gestiProy",
-    "hijo":["proyInven"]
-  },
-  {
-    "id": "etica",
-    "hijo":["ch1"]
-  },
-  {
-    "id": "cet5",
-    "hijo":[]
-  }  
-]
+        "id": "analisis2",
+        "hijo": ["analisis1"]
+    },
+    {
+        "id": "analisis1",
+        "hijo": []
+    },
+    {
+        "id": "gal1",
+        "hijo": []
+    },
+    {
+        "id": "fisica1",
+        "hijo": []
+    },
+    {
+        "id": "intRob",
+        "hijo": []
+    },
+    {
+        "id": "intIng",
+        "hijo": []
+    },
+    {
+        "id": "gal2",
+        "hijo": ["gal1"]
+    },
+    {
+        "id": "fisica2",
+        "hijo": []
+    },
+    {
+        "id": "matlab",
+        "hijo": []
+    },
+    {
+        "id": "funQui",
+        "hijo": []
+    },
+    {
+        "id": "caluclo",
+        "hijo": ["analisis2", "gal2"]
+    },
+    {
+        "id": "analisis3",
+        "hijo": ["analisis2", "gal2"]
+    },
+    {
+        "id": "mecanica1",
+        "hijo": ["fisica1", "analisis1"]
+    },
+    {
+        "id": "cad",
+        "hijo": []
+    },
+    {
+        "id": "labInd",
+        "hijo": []
+    },
+    {
+        "id": "ch1",
+        "hijo": []
+    },
+    {
+        "id": "prob",
+        "hijo": ["analisis2"]
+    },
+    {
+        "id": "mecanica2",
+        "hijo": ["mecanica1"]
+    },
+    {
+        "id": "termodinamica",
+        "hijo": ["analisis1"]
+    },
+    {
+        "id": "resMat",
+        "hijo": ["mecanica1"]
+    },
+    {
+        "id": "ayudaTecnica",
+        "hijo": ["cad"]
+    },
+    {
+        "id": "eco",
+        "hijo": []
+    },
+    {
+        "id": "comProf",
+        "hijo": []
+    },
+    {
+        "id": "ivestOper",
+        "hijo": ["prob"]
+    },
+    {
+        "id": "electroMag",
+        "hijo": ["fisica2", "analisis3"]
+    },
+    {
+        "id": "pasantia1",
+        "hijo": []
+    },
+    {
+        "id": "resMat2",
+        "hijo": ["resMat"]
+    },
+    {
+        "id": "ceinIngMat",
+        "hijo": ["funQui"]
+    },
+    {
+        "id": "labInd2",
+        "hijo": ["labInd"]
+    },
+    {
+        "id": "contYcost",
+        "hijo": []
+    },
+    {
+        "id": "ivestOper2",
+        "hijo": ["ivestOper"]
+    },
+    {
+        "id": "Electrotecnia1",
+        "hijo": ["electroMag"]
+    },
+    {
+        "id": "mecFludis",
+        "hijo": ["analisis3", "pasantia1", "termodinamica"]
+    },
+    {
+        "id": "Metalurgia",
+        "hijo": ["ceinIngMat"]
+    },
+    {
+        "id": "teologia",
+        "hijo": []
+    },
+    {
+        "id": "cet1",
+        "hijo": []
+    },
+    {
+        "id": "gestCal",
+        "hijo": []
+    },
+    {
+        "id": "operProd1",
+        "hijo": ["ivestOper"]
+    },
+    {
+        "id": "Electrotecnia2",
+        "hijo": ["Electrotecnia1"]
+    },
+    {
+        "id": "pasantia2",
+        "hijo": []
+    },
+    {
+        "id": "transfCalor",
+        "hijo": ["termodinamica"]
+    },
+    {
+        "id": "ingAmbiental",
+        "hijo": []
+    },
+    {
+        "id": "maq1",
+        "hijo": ["mecFludis", "mecanica2"]
+    },
+    {
+        "id": "cet2",
+        "hijo": []
+    },
+    {
+        "id": "maq2",
+        "hijo": ["pasantia2", "transfCalor"]
+    },
+    {
+        "id": "Electronica",
+        "hijo": ["electroMag"]
+    },
+    {
+        "id": "prosUnitarios",
+        "hijo": ["funQui"]
+    },
+    {
+        "id": "labInd3",
+        "hijo": ["labInd2"]
+    },
+    {
+        "id": "ch2",
+        "hijo": []
+    },
+    {
+        "id": "autoControlIndustrial",
+        "hijo": ["Electronica"]
+    },
+    {
+        "id": "diseElementMaquinas",
+        "hijo": ["resMat2"]
+    },
+    {
+        "id": "pasantia3",
+        "hijo": []
+    },
+    {
+        "id": "maq3",
+        "hijo": ["maq2"]
+    },
+    {
+        "id": "proyInven",
+        "hijo": ["contYcost"]
+    },
+    {
+        "id": "cet3",
+        "hijo": []
+    },
+    {
+        "id": "cet4",
+        "hijo": []
+    },
+    {
+        "id": "proyInd",
+        "hijo": ["Electrotecnia2", "maq3"]
+    },
+    {
+        "id": "elemGestion",
+        "hijo": ["eco"]
+    },
+    {
+        "id": "gestiProy",
+        "hijo": ["proyInven"]
+    },
+    {
+        "id": "etica",
+        "hijo": ["ch1"]
+    },
+    {
+        "id": "cet5",
+        "hijo": []
+    }
+];
 
-*/
-;
-let materias = [{"id": "analisis2", "hijo": ["analisis1"]}];
+//let materias = [{"id": "analisis2", "hijo": ["analisis1"]}];
 const mouseTarget = document.getElementsByClassName("btn");
-alert("esta entrando en la jsSheet");
 
-console.log(mouseTarget);
-
+console.log(mouseTarget.length);
 
 function pintar(idpadre) {
+
     for (i = 0; i < materias.length; i++) {
 
         if (materias[i].id === idpadre) {
 
             for (j = 0; j < materias[i].hijo.length; j++) {
-                document.getElementById(materias[i].hijo[j]).style.backgroundColor = "rgba(255,99,71,0.5)";
-                document.getElementById(materias[i].hijo[j]).style.boxShadow = "0px 0px 0px 0.2rem rgba(220,20,60,0.5)";
+                document.getElementById(idpadre).style.backgroundColor ="#e2e6ea"
+                if (materias[i].hijo[j] !== null) {
+                    console.log("pinta");
+
+                    document.getElementById(materias[i].hijo[j]).style.backgroundColor = "rgba(255,99,71,0.5)";
+                    document.getElementById(materias[i].hijo[j]).style.boxShadow = "0px 0px 0px 0.2rem rgba(220,20,60,0.5)";
+                }
             }
         }
     }
 }
 
 function despintar(idpadre) {
+
     for (i = 0; i < materias.length; i++) {
         if (materias[i].id === idpadre)
             for (j = 0; j < materias[i].hijo.length; j++) {
-                document.getElementById(materias[i].hijo[j]).style.backgroundColor =" #e2e6ea";
-                document.getElementById(materias[i].hijo[j]).style.boxShadow = "0px 0px 0px 0.0rem rgba(216,217,219,0.5)";
+                document.getElementById(idpadre).style.backgroundColor ="#f8f9fa"
+                if (materias[i].hijo[j] !== null){
+                    console.log("despinta");
+
+                    document.getElementById(materias[i].hijo[j]).style.backgroundColor = "#f8f9fa";
+                    document.getElementById(materias[i].hijo[j]).style.boxShadow = "0px 0px 0px 0.0rem rgba(216,217,219,0.5)";
+                }
             }
     }
 }
-function informar(idtemp){
-  alert(idtemp);
+
+
+function setValue(value) {
+    document.getElementById('value').value = value;
+}
+
+function informar(idtemp) {
+    var i, l, options = [{
+            value: 'first',
+            text: 'First'
+        }, {
+            value: 'second',
+            text: 'Second'
+        }],
+        newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");
+
+    newWindow.document.write("<select onchange='window.opener.setValue(this.value);'>");
+    for(i=0,l=options.length; i<l; i++) {
+        newWindow.document.write("<option value='"+options[i].value+"'>");
+        newWindow.document.write(options[i].text);
+        newWindow.document.write("</option>");
+    }
+    newWindow.document.write("</select>");
 }
 
 /*
@@ -286,6 +329,7 @@ function loop(){
 loop()
 
 */
+
 idPadre = null;
 
 
