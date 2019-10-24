55f1904f98dcb0a58b48c44127920f59873d6153
jabuarab <alasjan1@gmail.com>
2019-10-24 10:35:36 -0300

    asda

--- jsc/main.js
+++ jsc/main.js
@@ -291,22 +291,7 @@ function setValue(value) {
 }
 
 function informar(idtemp) {
-    var i, l, options = [{
-            value: 'first',
-            text: 'First'
-        }, {
-            value: 'second',
-            text: 'Second'
-        }],
-        newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");
-
-    newWindow.document.write("<select onchange='window.opener.setValue(this.value);'>");
-    for(i=0,l=options.length; i<l; i++) {
-        newWindow.document.write("<option value='"+options[i].value+"'>");
-        newWindow.document.write(options[i].text);
-        newWindow.document.write("</option>");
-    }
-    newWindow.document.write("</select>");
+alert(idtemp)
 }
 
 /*
