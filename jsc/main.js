2078baadb1947e7cfd21fb74e96675a78d693811
IgnacioCogliatti <jcogliatti@correo.um.edu.uy>
2019-10-25 12:22:09 -0300

    animation

--- jsc/main.js
+++ jsc/main.js
@@ -309,7 +309,7 @@ function setValue(value) {
     newWindow.document.write("</select>");
 }*/
 function informar(idpadre) {
-
+if (idpadre != "cets"){
 		
     for (i = 0; i < materias.length; i++) {
 
@@ -319,14 +319,25 @@ function informar(idpadre) {
         	popup = document.getElementById('popup'),
         	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
         	overlay.classList.add('active');
+        	popup.classList.add('active');
 
         }	
 
     }
 }
+if (idpadre == "cets") {
+			var btnAbrirPopup = document.getElementById('idpadre'),
+        	overlay = document.getElementById('overlay'),
+        	popup = document.getElementById('popup'),
+        	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+        	overlay.classList.add('active');
+
+}
+}
 function cerrar(){
 
-	overlay.classList.remove("active");	
+	overlay.classList.remove("active");
+	popup.classList.remove("active")	
 
 }
 /*btnCerrarPopup.addEventListener('click',function()){
@@ -918,56 +929,9 @@ mouseTarget[55].addEventListener("mouseleave", e => {
 mouseTarget[55].addEventListener("click", event => {
     informar(mouseTarget[55].id);
 });
-//56
-mouseTarget[56].addEventListener("mouseenter", e => {
-    pintar(mouseTarget[56].id);
-});
-mouseTarget[56].addEventListener("mouseleave", e => {
-    despintar(mouseTarget[56].id);
-});
-mouseTarget[56].addEventListener("click", event => {
-    informar(mouseTarget[56].id);
-});
-//57
-mouseTarget[57].addEventListener("mouseenter", e => {
-    pintar(mouseTarget[57].id);
-});
-mouseTarget[57].addEventListener("mouseleave", e => {
-    despintar(mouseTarget[57].id);
-});
-mouseTarget[57].addEventListener("click", event => {
-    informar(mouseTarget[57].id);
-});
-//58
-mouseTarget[58].addEventListener("mouseenter", e => {
-    pintar(mouseTarget[58].id);
-});
-mouseTarget[58].addEventListener("mouseleave", e => {
-    despintar(mouseTarget[58].id);
-});
-mouseTarget[58].addEventListener("click", event => {
-    informar(mouseTarget[58].id);
-});
-//59
-mouseTarget[59].addEventListener("mouseenter", e => {
-    pintar(mouseTarget[59].id);
-});
-mouseTarget[59].addEventListener("mouseleave", e => {
-    despintar(mouseTarget[59].id);
-});
-mouseTarget[59].addEventListener("click", event => {
-    informar(mouseTarget[59].id);
-});
-//60
-mouseTarget[60].addEventListener("mouseenter", e => {
-    pintar(mouseTarget[60].id);
-});
-mouseTarget[60].addEventListener("mouseleave", e => {
-    despintar(mouseTarget[60].id);
-});
-mouseTarget[60].addEventListener("click", event => {
-    informar(mouseTarget[60].id);
-});
+getElementById("cets").addEventListener("click",e=>{
+	informar("cets")
+})
 
 
 
